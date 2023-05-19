d49d5b83d6c281727d48bd7f6b0359aeaf71edd8
littlephillips <phillipkinuthia72@gmail.com>
2023-05-19 17:12:43 +0300

    form validations

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -18,19 +18,24 @@ function App() {
     name: "username",
     type: "text",
     placeholder:"Username",
-    label: "Username"
+    errorMessage:"Username should be 3-16 characters and shouldn't include any special characters!",
+    label: "Username",
+    pattern: "^[A-Za-z0-9]{3,16}$",
+    required: true,
   },
   {
     id:2,
     name: "email",
-    type: "text",
+    type: "email",
     placeholder:"Email",
-    label: "email"
+    errorMessage:"It should be an email address",
+    label: "email",
+    required: true,
   },
   {
     id:3,
     name: "birthday",
-    type: "text",
+    type: "date",
     placeholder:"Birthday",
     label: "Birthday"
   },
@@ -39,15 +44,21 @@ function App() {
     name: "password",
     type: "password",
     placeholder:"Password",
-    label: "Password"
+    errorMessage:"Password should be 8-20 characters and include at least 1 letter, 1 number and i special character!",
+    label: "Password",
+    pattern:"^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*)][a-zA-Z0-9!@#$%^&*]{8,20}$",
+    required: true,
   },
   {
     id:5,
     name: "confirmPassword",
     type: "password",
     placeholder:"Confirm Password",
-    label: "Confirm Password"
-  }]
+    errorMessage:"Password don't match",
+    label: "Confirm Password",
+    pattern: values.password,
+    required: true,
+  }];
 
 
   const handleSubmit = (e) => {
@@ -59,10 +70,11 @@ function App() {
   }
 
   console.log(values);
-  
+
   return (
     <div className="app">
       <form onSubmit={handleSubmit}>
+        <h1>Register</h1>
         {inputs.map((input) => (
           <FormInput 
           key={input.id} 
--- src/components/Form/FormInput.js
+++ src/components/Form/FormInput.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState} from 'react';
 import '../../css/form.css';
 
 function FormInput( props ) {
-  const { label, onChange, id, ...inputProps} = props;
+  const [focused, setFocused] = useState(false);
+  const { label,errorMessage, onChange, id, ...inputProps} = props;
 
-
+  const handleFocus = (e) => {
+    setFocused(true);
+  }
 
   return (
     <div className="formInput">
         <label> {label} </label>
-        <input {...inputProps} onChange={onChange} />
+        <input 
+          {...inputProps} 
+          onChange={onChange} 
+          onBlur={handleFocus} 
+          onFocus={() => 
+            inputProps.name === "confirmPassword" && setFocused(true)}
+          focused={focused.toString()}
+        />
+        <span>{errorMessage}</span>
     </div>
   )
 }
